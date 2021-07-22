267ec7e61a923c6c70b125147c786b032dcee8fe
technobaboo <technobaboo@gmail.com>
2021-07-21 19:50:42 -0500

    Add image embed

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,6 +29,7 @@ export default function Home() {
 	const {siteConfig} = useDocusaurusContext();
 	return (
 		<Layout
+			image="img/icon.gif"
 			description="XR Display Server">
 			<HomepageHeader />
 			<main>
