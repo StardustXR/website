befa3b5179856274718aea390329f8d323b8407f
Nova <technobaboo@gmail.com>
2022-12-16 12:44:34 -0500

    link rel back to mastodon for verification of webstie

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ function HomepageHeader() {
 	return (
 		<header className={clsx('hero hero--primary', styles.heroBanner)}>
 			<div className="container">
+				<link rel="me" href="https://fosstodon.org/@stardustxr"></link>
 				<h1 className="hero__title">{siteConfig.title}</h1>
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
 				<div className={styles.buttons}>
